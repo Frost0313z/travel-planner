d9554c62d482115ea7073a538dda74e0db47ecfb
Drako_Dev <msong7618@gmail.com>
2026-08-05 17:10:13 +0900

    chore: 프로젝트 구조 초기화

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+
+from travel_planner.core import run
+
+if __name__ == "__main__":
+    if hasattr(sys.stdout, "reconfigure"):
+        sys.stdout.reconfigure(encoding="utf-8")
+        sys.stdin.reconfigure(encoding="utf-8")
+    run()
